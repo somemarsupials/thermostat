0ebadc2aa1f7e29924bfac52bc5411c2dcbf6cc6
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-21 14:20:01 +0000

    added up and down fucntions to thermostat" git push

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,4 +12,16 @@ describe('Thermostat', function() {
       expect(thermostat.temperature).toEqual(20);
     });
   });
+  describe('#up', function() {
+    it('should increase with an up function', function() {
+      thermostat.up(1);
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
+  describe('#down', function() {
+    it('should decrease with a down function', function() {
+      thermostat.down(1);
+      expect(thermostat.temperature).toEqual(19);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -5,3 +5,11 @@ var INIT_TEMP = 20;
 function Thermostat(initial) {
   this.temperature = initial || INIT_TEMP;
 };
+
+Thermostat.prototype.up = function (number) {
+  this.temperature += number;
+};
+
+Thermostat.prototype.down = function (number) {
+  this.temperature -= number;
+};
