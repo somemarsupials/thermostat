3b0b17f59aeda84c0c83d9d150df1e2e903c25d2
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-21 15:14:49 +0000

    implements temperature limits and power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,26 +2,70 @@
 
 describe('Thermostat', function() {
   var thermostat;
+  var limits;
 
   beforeEach(function() {
-    thermostat = new Thermostat();
+    thermostat = new Thermostat(20, limits);
   });
 
   describe('#new', function() {
     it('should start at 20 degrees', function() {
       expect(thermostat.temperature).toEqual(20);
     });
+
+    it('should start in power-saving mode', function() {
+      expect(thermostat.powersaving).toEqual(true);
+    });
   });
+
+  describe('#min', function() {
+    it('should get minimum temperature', function() {
+      expect(thermostat.min()).toEqual(10);
+    });
+  });
+
+  describe('#max', function() {
+    it('should get maximum temperature when not in PS mode', function() {
+      thermostat.powersaving = false;
+      expect(thermostat.max()).toEqual(32);
+    });
+
+    it('should get maximum temperature when in PS mode', function() {
+      expect(thermostat.max()).toEqual(25);
+    });
+  });
+
   describe('#up', function() {
     it('should increase with an up function', function() {
       thermostat.up(1);
       expect(thermostat.temperature).toEqual(21);
     });
+
+    describe('in power saving mode', function() {
+      it('cannot increase above 25 degrees', function() {
+        thermostat.up(100);
+        expect(thermostat.temperature).toEqual(25);
+      });
+    });
+
+    describe('not in power saving mode', function() {
+      it('cannot increase above 32 degrees', function() {
+        thermostat.powersaving = false;
+        thermostat.up(100);
+        expect(thermostat.temperature).toEqual(32);
+      });
+    });
   });
+
   describe('#down', function() {
     it('should decrease with a down function', function() {
       thermostat.down(1);
       expect(thermostat.temperature).toEqual(19);
+    });
+
+    it('cannot reduce below 10 degrees', function() {
+      thermostat.down(100);
+      expect(thermostat.temperature).toEqual(10);
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,15 +1,30 @@
 'use strict';
 
 var INIT_TEMP = 20;
+var MAX_TEMP = 32;
+var MAX_PS_TEMP = 25;
+var MIN_TEMP = 10;
 
-function Thermostat(initial) {
-  this.temperature = initial || INIT_TEMP;
+function Thermostat() {
+  this.temperature = INIT_TEMP;
+  this._min =  MIN_TEMP;
+  this._max = MAX_TEMP;
+  this._ps_max = MAX_PS_TEMP;
+  this.powersaving = true;
+};
+
+Thermostat.prototype.min = function() {
+  return this._min;
+};
+
+Thermostat.prototype.max = function() {
+  return this.powersaving ? this._ps_max : this._max;
 };
 
 Thermostat.prototype.up = function (number) {
-  this.temperature += number;
+  this.temperature = Math.min(this.max(), this.temperature + number);
 };
 
 Thermostat.prototype.down = function (number) {
-  this.temperature -= number;
+  this.temperature = Math.max(this.min(), this.temperature - number);
 };
